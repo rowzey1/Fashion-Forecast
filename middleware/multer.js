132360902b5b9ca92ddc96500e4c522b50568d75
Rosy Basil-Kalu <basilkalurosy@gmail.com>
2024-12-10 13:29:22 -0500

    Updated multer config

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,14 +1,27 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dir = 'public/uploads/wardrobe';
+    fs.mkdirSync(dir, { recursive: true });
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (!ext.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
 
 module.exports = multer({
-  storage: multer.diskStorage({}),
-  fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      cb(new Error("File type is not supported"), false);
-      return;
-    }
-    cb(null, true);
-  },
+  storage: storage,
+  fileFilter: fileFilter,
 });
--- routes/wardrobeRoutes.js
+++ routes/wardrobeRoutes.js
@@ -4,31 +4,9 @@ const { addClothingItem, getWardrobe, suggestOutfits, deleteClothingItem,suggest
 const isLoggedIn = require('../Fashion-Forecast/middleware/auth'); 
 const GeoLocationService = require("../services/geoLocationservice");
 const WeatherService = require("../services/weatherService");
-const multer = require('multer');
-
+const upload = require('../middleware/multer');
 const router = express.Router();
 
-// Multer setup for file uploads
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            const dir = 'public/uploads/wardrobe';
-            fs.mkdirSync(dir, { recursive: true });
-            cb(null, dir)
-        },
-        filename: (req, file, cb) => {
-            cb(null, `${Date.now()}-${file.originalname}`)
-        }
-    }),
-    fileFilter: (req, file, cb) => {
-        // Accept images only
-        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-            return cb(new Error('Only image files are allowed!'), false);
-        }
-        cb(null, true);
-    }
-    
-});
 
 // Wardrobe routes
 router.post('/add', isLoggedIn, upload.single('image'), addClothingItem);
